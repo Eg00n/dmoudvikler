912fccfaa93a6082982ebedf1f3fd5d4330c0ae4
Jakob Egon Jespersen <Jespersen92@gmail.com>
2017-03-13 17:59:07 +0100

    added project to repository

---
+++ DmoUdvikler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DmoUdvikler.MvcApplication" Language="C#" %>
---
+++ DmoUdvikler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DmoUdvikler.Startup))]
+namespace DmoUdvikler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
